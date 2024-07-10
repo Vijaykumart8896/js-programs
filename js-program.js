da388f7aaf2e936546b4159fcbc91e847b8f1bb4
vijaykumart8896 <vijaykumart8896@gmail.com>
2024-07-10 19:23:02 +0530

    added new functions

--- js-program.js
+++ js-program.js
@@ -1,1 +1,24 @@
-console.log("Welcome to the Js-programs")
+console.log("Welcome to the Js-programs");
+
+document.addEventListener("visibilitychange", () => {
+  document.hidden
+    ? (document.title = "Please dont go💔")
+    : (document.title = "JS programs");
+});
+
+// ========== how to add time in web pages ===============
+let time = document.getElementById("time");
+console.log(time);
+
+function getTime() {
+  setInterval(() => {
+    let currentTime = new Date(); // Get the current time
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+
+    time.innerHTML = `Time ${hours}:${minutes}:${seconds}`;
+  }, 1000); 
+}
+
+document.addEventListener("DOMContentLoaded", getTime)
